94ed6329663c34e5d6643502a16132760b69a8a2
Nublnv <cp7reg@gmail.com>
2021-05-13 18:21:00 +0300

    Simple buttons

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button yesButton, noButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        yesButton = findViewById(R.id.buttonYes);
+        noButton = findViewById(R.id.buttonNo);
+        yesButton.setOnClickListener(this::yesButton_onClick);
+        noButton.setOnClickListener(this::noButton_onClick);
+    }
+
+    public void yesButton_onClick(View view)
+    {
+        Toast.makeText(MainActivity.this,R.string.correct,Toast.LENGTH_SHORT).show();
+    }
+
+    public void noButton_onClick(View view)
+    {
+        Toast.makeText(MainActivity.this,R.string.incorrect,Toast.LENGTH_SHORT).show();
     }
 }
